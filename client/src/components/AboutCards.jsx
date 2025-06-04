e0ab08c7aa3daa7a2ae537b93cea8b7f4bccf748
Ryan Fann <141375216+8BitGinger@users.noreply.github.com>
2025-06-04 00:10:29 -0500

    Update About Me Page
    Updated the About me page with sections to remove Adrian and Pets and add professional about me section

--- client/src/components/AboutCards.jsx
+++ client/src/components/AboutCards.jsx
@@ -3,13 +3,14 @@ import { fadeIn } from '../util/variants';
 import { motion } from 'framer-motion';
 import '../assets/css/about.css';
 import legoAvatar from '../assets/images/profHeadshot.jpg';
-import beachUs from '../assets/images/beachUs.jpg';
+import work from '../assets/images/Work2.jpg';
+import gadget from '../assets/images/gadget.jpg';
 
 const AboutCards = () => {
   return (
     <>
       <motion.article
-        variants={fadeIn('up', 0.5)}
+        variants={fadeIn('left', 0.5)}
         initial="hidden"
         whileInView={'show'}
         viewport={{ once: false, amount: 0.3 }}
@@ -33,7 +34,27 @@ const AboutCards = () => {
       </motion.article>
 
       <motion.article
-        variants={fadeIn('up', 0.5)}
+        variants={fadeIn('right', 0.5)}
+        initial="hidden"
+        whileInView={'show'}
+        viewport={{ once: false, amount: 0.3 }}
+        className="about-container"
+        id="about"
+      >
+        <h2>Professional Skills</h2>
+        <div className="about-text">
+          <div className="para-2">
+            <p className="about-2">
+              Currently, I leverage my technical expertise as an IT Engineer for KAMC and KLBK TV stations. From selling cutting-edge IT equipment at CompUSA to providing essential tech support for Apple and DirecTV, I've always been immersed in the world of technology, however. Through it all, I discovered my ultimate passion: the transformative power of code!
+              
+            </p>
+            <img loading="lazy" src={work} alt="us at the beach" />
+          </div>
+        </div>
+      </motion.article>
+
+            <motion.article
+        variants={fadeIn('left', 0.5)}
         initial="hidden"
         whileInView={'show'}
         viewport={{ once: false, amount: 0.3 }}
@@ -43,16 +64,14 @@ const AboutCards = () => {
         <h2>Hobbies and Interests</h2>
         <div className="about-text">
           <div className="para-2">
+            <img loading="lazy" src={gadget} alt="us at the beach" />
             <p className="about-2">
-              Outside the world of code, I unwind with my fantastic girlfriend,
-              Adrian, and our awesome furry &amp; feathery companions. When I
-              need a coding detox, you might find me conquering virtual worlds
-              on my Xbox or delving into fantastical tales with friends through
-              tabletop RPGs. I&apos;m often vibin&apos; to one of my
-              meticulously crafted playlists on Apple Music, and when I need a
-              dose of nostalgia, I love streaming classic sci-fi shows.
+              When I need a coding detox, you might find me conquering virtual worlds
+              on my Xbox/Switch, delving into fantastical tales with friends through
+              tabletop RPGs, or when I need a
+              dose of nostalgia: Streaming classic sci-fi Movies and Tv Shows!
             </p>
-            <img loading="lazy" src={beachUs} alt="us at the beach" />
+            
           </div>
         </div>
       </motion.article>
