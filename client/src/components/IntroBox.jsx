e31c2faed0d4848f637b84ba3e68a6e0abbd0abf
Ryan Fann <141375216+8BitGinger@users.noreply.github.com>
2025-08-03 23:48:39 -0500

    Sizing correction for medium sized screens
    Corrected medium screen size scaling and positioning.

--- client/src/components/IntroBox.jsx
+++ client/src/components/IntroBox.jsx
@@ -44,7 +44,7 @@ const IntroBox = () => {
         initial="hidden"
         whileInView={'show'}
         viewport={{ once: false, amount: 0.7 }}
-        className="flex flex-row"
+        className="flex  flex-row"
       >
         <span className="intro-desc">I am </span>
         <TypeAnimation
